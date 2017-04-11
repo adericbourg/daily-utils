2b177e10554fdaef4e3bdec6f55c70b9b87ae386
Alban Dericbourg <alban@dericbourg.net>
2017-04-11 14:15:36 +0200

    Cleanup Twitter dependencies
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- project/Twitter.scala
+++ project/Twitter.scala
@@ -2,8 +2,7 @@ import sbt._
 
 object Twitter {
 
-  def utilCoreDependency(scalaVersion: String): ModuleID = scalaVersion match {
-    case _ if scalaVersion.startsWith("2.10.") => "com.twitter" %% "util-core" % "6.34.0"
-    case _ => "com.twitter" %% "util-core" % "6.42.0"
-  }
+  def utilCoreDependency(scalaVersion: String): ModuleID =
+    if (scalaVersion.startsWith("2.10.")) { "com.twitter" %% "util-core" % "6.34.0" }
+    else { "com.twitter" %% "util-core" % "6.42.0" }
 }
